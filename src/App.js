41f6842b714467c5ebcb01cde402ad2f1bf46e00
Tracy Tucker <tracytuckeremail@gmail.com>
2022-04-19 18:57:09 -0500

    Adding logo and Nav styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-import Navigation from "./routes/home/navigation/navigation.component";
+import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
   );
