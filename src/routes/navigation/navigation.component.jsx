210f8a7166be851fea7ec123054931254220f071
Tracy Tucker <tracytuckeremail@gmail.com>
2022-04-26 18:38:34 -0500

    Adding cart context to toggle cart dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,6 +4,7 @@ import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
@@ -13,6 +14,7 @@ const Navigation = () => {
   // setCurrentUser no longer needed. Leveraging onAuthStateChanged instead
   // currentUser still needed for sign in/sign out toggle
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   // no longer needed. Leveraging onAuthStateChanged instead
   // const signOutHandler = async () => {
@@ -42,7 +44,7 @@ const Navigation = () => {
           )}
           <CartIcon />
         </div>
-        <CartDropdown />
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
@@ -50,3 +52,6 @@ const Navigation = () => {
 };
 
 export default Navigation;
+
+// isCartOpen && --> The && is evaluating the truthiness of the statement
+// for this to be true, both the left and right side of the && must be a truthy value
