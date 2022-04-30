ace9a24ab87fc8bcda886a5f0bcf153e7ccae824
Tracy Tucker <tracytuckeremail@gmail.com>
2022-04-30 15:36:40 -0500

    Fixing styling for directory and category-preview

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,12 +1,12 @@
 import "./category-preview.styles.scss";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import ProductCard from "../product-card/product-card.component";
 
 const CategoryPreview = ({ title, products }) => {
   return (
     <div className="category-preview-container">
       <h2>
-        <span>{title.toUpperCase()}</span>
+        <Link to={title}>{title.toUpperCase()}</Link>
       </h2>
       <div className="preview">
         {products
--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,21 +1,21 @@
-import "./category-item.styles.scss";
+import "./directory-item.styles.scss";
 
-const CategoryItem = ({ category }) => {
+const DirectoryItem = ({ category }) => {
   const { title, imageUrl } = category;
   return (
-    <div className="category-container">
+    <div className="directory-item-container">
       <div
         className="background-image"
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
       />
-      <div className="category-body-container">
+      <div className="body">
         <h2>{title}</h2>
-        <p>Show Now</p>
+        <p>Shop Now</p>
       </div>
     </div>
   );
 };
 
-export default CategoryItem;
+export default DirectoryItem;
